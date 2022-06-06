12ec7266120b491da171db37042eca0b9ffc8169
fi-ya <safiaali@hotmail.com>
2022-06-06 18:26:49 +0100

    test: remove processClientRequestSuccessfully from requestParserTest
    logic handled in requestBuilder

--- src/test/java/org/httpserver/request/RequestParserTest.java
+++ src/test/java/org/httpserver/request/RequestParserTest.java
@@ -27,26 +27,4 @@ class RequestParserTest {
         assertEquals("HTTP/1.1", actualMap.get("httpVersion"));
     }
 
-    @Test
-    void processClientRequestSuccessfully() throws IOException {
-        BufferedReader mockClientRequestReader = mock(BufferedReader.class);
-        String requestLineStub = "GET /simple_get_with_body HTTP/1.1";
-        LinkedHashMap<String, String> requestLineMapStub = new LinkedHashMap<>() {{
-            put("httpMethod", "GET");
-            put("requestTarget", "/simple_get_with_body");
-            put("httpVersion", "HTTP/1.1");
-        }};
-
-        LinkedHashMap<String, String> requestHeadersMapStub = new LinkedHashMap<>();
-        String requestBodyStub = "";
-
-        when(mockClientRequestReader.readLine()).thenReturn(requestLineStub);
-        RequestParser requestParser = new RequestParser();
-        Request mockRequest = new Request(requestLineMapStub, requestHeadersMapStub, requestBodyStub);
-
-        String expectedResponse = "HTTP/1.1 200 OK\r\n\r\nHello world";
-        String actualResponse = requestParser.responseBuilder(mockRequest);
-
-        assertEquals(expectedResponse, actualResponse);
-    }
 }
