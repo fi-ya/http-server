b403181b69e986210bd3126b6ee40e735768f3ec
fi-ya <safiaali@hotmail.com>
2022-06-06 14:50:49 +0100

    refactor: simple_get_with_body handler returns correct response

--- src/main/java/org/httpserver/handler/SimpleGetWithBodyHandler.java
+++ src/main/java/org/httpserver/handler/SimpleGetWithBodyHandler.java
@@ -5,27 +5,24 @@ import org.httpserver.response.Response;
 import org.httpserver.response.ResponseBuilder;
 import org.httpserver.server.HttpMethod;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class MethodNotAllowedHandler implements Handler{
+public class SimpleGetWithBodyHandler implements Handler {
     @Override
     public List<String> allowedHttpMethods() {
-        return Arrays.asList(HttpMethod.HEAD, HttpMethod.OPTIONS);
+        return List.of(HttpMethod.GET);
     }
 
-    @Override
     public Response responseBuilder(Request request) {
         String SP = " ";
         String CRLF = "\r\n";
-        String statusCode = "405";
-        String statusText = "Method Not Allowed";
+        String statusCode = "200";
+        String statusText = "OK";
         String responseStatusLine = request.getHttpVersion() + SP + statusCode + SP + statusText + CRLF;
-        String responseHeaders = "Allow: " + allowedHttpMethods().toString() +  CRLF;
-        String responseBody = "";
+        String responseHeaders = "" + CRLF;
+        String responseBody = "Hello world";
 
         ResponseBuilder responseBuilder = new ResponseBuilder();
-
         responseBuilder.setResponseStatusLine(responseStatusLine);
         responseBuilder.setResponseHeaders(responseHeaders);
         responseBuilder.setResponseBody(responseBody);
--- src/main/java/org/httpserver/server/Router.java
+++ src/main/java/org/httpserver/server/Router.java
@@ -1,9 +1,6 @@
 package org.httpserver.server;
 
-import org.httpserver.handler.Handler;
-import org.httpserver.handler.HeadRequestHandler;
-import org.httpserver.handler.MethodNotAllowedHandler;
-import org.httpserver.handler.SimpleGetHandler;
+import org.httpserver.handler.*;
 import org.httpserver.request.Request;
 
 import java.util.HashMap;
@@ -12,37 +9,33 @@ import java.util.Map;
 
 public class Router {
 
-    private final Map<String, Handler> resourceAndHandler;
+    private final Map<String, Handler> resourceAndHandlerMap;
 
     public Router() {
-        this.resourceAndHandler = createResourceAndHandler();
+        this.resourceAndHandlerMap = createResourceAndHandlerMap();
     }
 
-    public Map<String, Handler> createResourceAndHandler() {
+    public Map<String, Handler> createResourceAndHandlerMap() {
         return new HashMap<>() {{
             put("/simple_get", new SimpleGetHandler());
+            put("/simple_get_with_body", new SimpleGetWithBodyHandler());
             put("/head_request", new HeadRequestHandler());
-//                put("/simple_get_with_body", "new SimpleGetHandler()");
-//                put("/redirect", "new RedirectHandler()");
-//                put("/echo_body", "new PostHandler()");
-//                put("/method_options", "new OptionsHandler1()");
-//                put("/method_options2", "new OptionsHandler2()");
         }};
     }
 
     public Handler getHandler(Request request) {
-        if (resourceTargetExists(request) && isMethodAllowed(request)) {
-            return resourceAndHandler.get(request.getRequestTarget());
+        if (resourceTargetExists(request) && isHttpMethodAllowed(request)) {
+            return resourceAndHandlerMap.get(request.getRequestTarget());
         }
         return new MethodNotAllowedHandler();
     }
 
     private boolean resourceTargetExists(Request request) {
-        return resourceAndHandler.containsKey(request.getRequestTarget());
+        return resourceAndHandlerMap.containsKey(request.getRequestTarget());
     }
 
-    private boolean isMethodAllowed(Request request) {
-        Handler handler = resourceAndHandler.get(request.getRequestTarget());
+    private boolean isHttpMethodAllowed(Request request) {
+        Handler handler = resourceAndHandlerMap.get(request.getRequestTarget());
         List<String> getAllowedMethods = handler.allowedHttpMethods();
 
         return getAllowedMethods.contains(request.getHttpMethod());
