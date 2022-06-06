862c49b451cafc7e9830a71b8cb782b7e79f7303
fi-ya <safiaali@hotmail.com>
2022-06-06 22:13:59 +0100

    test: sentClientResponseReadCorrectlyAndClosedClientSuccessfully

--- src/test/java/org/httpserver/client/ClientHandlerTest.java
+++ src/test/java/org/httpserver/client/ClientHandlerTest.java
@@ -1,9 +1,11 @@
 package org.httpserver.client;
 
+import org.httpserver.response.Response;
 import org.httpserver.server.StdOutServerLogger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -12,18 +14,20 @@ import static org.mockito.Mockito.*;
 
 class ClientHandlerTest {
 
-//    @Test
-//    void sentClientResponseReadCorrectlyAndClosedClientSuccessfully() throws IOException {
-//        StdOutServerLogger serverLogger = new StdOutServerLogger();
-//        Socket mockClientSocket = mock(Socket.class);
-//        ClientHandler clientHandler = new ClientHandler(mockClientSocket, serverLogger);
-//        PrintWriter mockClientResponseWriter = mock(PrintWriter.class);
-//        String mockResponse = "HTTP/1.1 200 OK";
-//
-//        clientHandler.sendResponse(mockResponse, mockClientResponseWriter);
-//
-//        verify(mockClientResponseWriter).write("HTTP/1.1 200 OK");
-//        verify(mockClientResponseWriter, times(1)).close();
-//        assertEquals(0, clientHandler.clientConnectionCounter);
-//    }
+    @Test
+    void sentClientResponseReadCorrectlyAndClosedClientSuccessfully() throws IOException {
+        StdOutServerLogger serverLogger = new StdOutServerLogger();
+        Socket mockClientSocket = mock(Socket.class);
+        ClientHandler clientHandler = new ClientHandler(mockClientSocket, serverLogger);
+        PrintWriter mockClientResponseWriter = mock(PrintWriter.class);
+        String mockResponseString = "HTTP/1.1 200 OK";
+        Response mockResponse = mock(Response.class);
+
+        when(mockResponse.stringFormatResponse()).thenReturn(mockResponseString);
+
+        clientHandler.sendResponse(mockResponse, mockClientResponseWriter);
+
+        verify(mockClientResponseWriter).write("HTTP/1.1 200 OK");
+        verify(mockClientResponseWriter, times(1)).close();
+    }
 }
