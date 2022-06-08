677f6d39993bcf4d254682c70e10b3362665d969
fi-ya <safiaali@hotmail.com>
2022-06-08 17:04:15 +0100

    test: getHandlerReturnsGetHandlerForHeadRequestRoute

--- src/test/java/org/httpserver/server/RouterTest.java
+++ src/test/java/org/httpserver/server/RouterTest.java
@@ -1,6 +1,7 @@
 package org.httpserver.server;
 
 import org.httpserver.handler.Handler;
+import org.httpserver.handler.HeadRequestHandler;
 import org.httpserver.handler.SimpleGetHandler;
 import org.httpserver.request.Request;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,24 @@ class RouterTest {
         SimpleGetHandler expectedHandler = new SimpleGetHandler();
 
         assertEquals(expectedHandler.getClass(), actualHandler.getClass());
+    }
 
+    @Test
+    void getHandlerReturnsGetHandlerForHeadRequestRoute() {
+
+        LinkedHashMap<String, String> requestLineStub = new LinkedHashMap<String, String>() {{
+            put("httpMethod", "HEAD");
+            put("requestTarget", "/head_request");
+            put("httpVersion", "HTTP/1.1");
+        }};
+        LinkedHashMap<String, String> requestHeadersStub = new LinkedHashMap<>();
+        String requestBodyStub = "";
+        Request requestMock = new Request(requestLineStub, requestHeadersStub, requestBodyStub);
+
+        Router router = new Router();
+        Handler actualHandler = router.getHandler(requestMock);
+        HeadRequestHandler expectedHandler = new HeadRequestHandler();
+
+        assertEquals(expectedHandler.getClass(), actualHandler.getClass());
     }
 }
