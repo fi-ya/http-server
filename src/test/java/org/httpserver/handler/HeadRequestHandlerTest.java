391b0cf07e3aed554689ab6ae82c987ed5a48674
fi-ya <safiaali@hotmail.com>
2022-06-09 13:48:12 +0100

    test: handleResponse_ReturnsResponseWith_ResponseStatusLineOnly

--- src/test/java/org/httpserver/handler/HeadRequestHandlerTest.java
+++ src/test/java/org/httpserver/handler/HeadRequestHandlerTest.java
@@ -1,10 +1,14 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HeadRequestHandlerTest {
 
@@ -19,7 +23,21 @@ class HeadRequestHandlerTest {
     }
 
     @Test
-    void handleResponse() {
+    void handleResponse_ReturnsResponseWith_ResponseStatusLineOnly() {
+        LinkedHashMap<String, String> requestLineStub = new LinkedHashMap<String, String>() {{
+            put("httpVersion", "HTTP/1.1");
+            put("httpMethod", "HEAD");
+            put("requestTarget", "/head_request");
+        }};
+        LinkedHashMap<String, String> requestHeadersStub = new LinkedHashMap<>();
+        String requestBodyStub = "";
+        Request mockRequest = new Request(requestLineStub, requestHeadersStub, requestBodyStub);
 
+        HeadRequestHandler headRequestHandler = new HeadRequestHandler();
+        Response actual = headRequestHandler.handleResponse(mockRequest);
+
+        assertEquals("HTTP/1.1 200 OK\r\n", actual.getResponseStatusLine());
+        assertEquals("\r\n", actual.getResponseHeaders());
+        assertTrue(actual.getResponseBody().isEmpty());
     }
 }
