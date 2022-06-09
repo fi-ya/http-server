62ebb47e1b6baa7c75fb5b8862e97a1c1b8858fa
fi-ya <safiaali@hotmail.com>
2022-06-09 14:31:00 +0100

    test: Response class get methods return correct values

---
+++ src/test/java/org/httpserver/response/ResponseTest.java
@@ -0,0 +1,43 @@
+package org.httpserver.response;
+
+import org.httpserver.request.Request;
+import org.httpserver.server.HttpMethod;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResponseTest {
+
+    private Response actualResponse;
+    @BeforeEach
+    void setup() {
+        String responseStatusLine = "HTTP/1.1 200 OK";
+        String responseHeaders = "";
+        String requestBody = "";
+
+       actualResponse = new Response(responseStatusLine, responseHeaders, requestBody);
+    }
+
+    @Test
+    void getResponseStatusLine() {
+        assertEquals("HTTP/1.1 200 OK", actualResponse.getResponseStatusLine());
+    }
+
+    @Test
+    void getResponseHeaders() {
+        assertEquals("", actualResponse.getResponseHeaders());
+    }
+
+    @Test
+    void getResponseBody() {
+        assertEquals("", actualResponse.getResponseBody());
+    }
+
+    @Test
+    void stringFormatResponse() {
+        assertEquals("HTTP/1.1 200 OK", actualResponse.stringFormatResponse());
+    }
+}
