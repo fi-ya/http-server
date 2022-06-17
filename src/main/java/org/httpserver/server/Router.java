63ce892d8807aeec614f3599afd41c5809592b76
fi-ya <safiaali@hotmail.com>
2022-06-17 13:04:09 +0100

    refactor: remove unused imports

--- src/main/java/org/httpserver/server/Router.java
+++ src/main/java/org/httpserver/server/Router.java
@@ -29,7 +29,7 @@ public class Router {
     public Handler getHandler(Request request) {
         if (resourceTargetExists(request) && isHttpMethodAllowed(request)) {
             return resourceAndHandlerMap.get(request.getRequestTarget());
-        } else if (resourceTargetExists(request) && !isHttpMethodAllowed(request)){
+        } else if (resourceTargetExists(request) && !isHttpMethodAllowed(request)) {
             return new MethodNotAllowedHandler();
         } else {
             return new PageNotFoundHandler();
