6ca6437077f96d5bc0e61e3f4aa631911070c1e2
fi-ya <safiaali@hotmail.com>
2022-06-17 10:21:25 +0100

    test: echo_body returnsResponseWithStatusLineAndBodyOnly

--- src/main/java/org/httpserver/handler/EchoBodyHandler.java
+++ src/main/java/org/httpserver/handler/EchoBodyHandler.java
@@ -2,19 +2,43 @@ package org.httpserver.handler;
 
 import org.httpserver.request.Request;
 import org.httpserver.response.Response;
+import org.httpserver.response.ResponseBuilder;
+import org.httpserver.response.StatusCode;
 import org.httpserver.server.HttpMethod;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class EchoBodyHandler implements Handler{
+public class EchoBodyHandler implements Handler {
     @Override
     public List<String> allowedHttpMethods() {
         return List.of(HttpMethod.POST.getHttpMethod());
     }
 
-    @Override
     public Response handleResponse(Request request) {
-        return null;
+        String CRLF = "\r\n";
+
+        String responseStatusLine = handleStatusLine(request) + CRLF;
+        String responseHeaders = handleHeaders() + CRLF;
+        String responseBody = handleBody(request);
+
+        ResponseBuilder responseBuilder = new ResponseBuilder();
+        return responseBuilder.buildResponse(responseStatusLine, responseHeaders, responseBody);
+    }
+
+    private String handleStatusLine(Request request) {
+        String SP = " ";
+        String statusCode = StatusCode.OK.getStatusCode();
+        String statusText = String.valueOf(StatusCode.OK);
+
+        return request.getHttpVersion() + SP + statusCode + SP + statusText;
+    }
+
+    private String handleHeaders() {
+        return "";
+    }
+
+    private String handleBody(Request request) {
+        return request.getRequestBody();
     }
 }
--- src/test/java/org/httpserver/handler/EchoBodyHandlerTest.java
+++ src/test/java/org/httpserver/handler/EchoBodyHandlerTest.java
@@ -1,6 +1,10 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,21 @@ class EchoBodyHandlerTest {
     }
 
     @Test
-    void handleResponse() {
+    void returnsResponseWithStatusLineAndBodyOnly() {
+        LinkedHashMap<String, String> requestLineStub = new LinkedHashMap<String, String>() {{
+            put("httpVersion", "HTTP/1.1");
+            put("httpMethod", "POST");
+            put("requestTarget", "/echo_body");
+        }};
+        LinkedHashMap<String, String> requestHeadersStub = new LinkedHashMap<>();
+        String requestBodyStub = "some body";
+        Request requestMock = new Request(requestLineStub, requestHeadersStub, requestBodyStub);
+        EchoBodyHandler echoBodyHandler = new EchoBodyHandler();
+
+        Response actualResponse = echoBodyHandler.handleResponse(requestMock);
+
+        assertEquals("HTTP/1.1 200 OK\r\n", actualResponse.getResponseStatusLine());
+        assertTrue(actualResponse.getResponseHeaders().isBlank());
+        assertEquals(requestBodyStub, actualResponse.getResponseBody());
     }
 }
