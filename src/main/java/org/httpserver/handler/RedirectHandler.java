ff0ad115afcd943ecd08934d879950dd205140d0
fi-ya <safiaali@hotmail.com>
2022-06-17 10:46:00 +0100

    test: redirect route returnsResponseWithStatusLineAndHeaderOnly

--- src/main/java/org/httpserver/handler/RedirectHandler.java
+++ src/main/java/org/httpserver/handler/RedirectHandler.java
@@ -2,6 +2,8 @@ package org.httpserver.handler;
 
 import org.httpserver.request.Request;
 import org.httpserver.response.Response;
+import org.httpserver.response.ResponseBuilder;
+import org.httpserver.response.StatusCode;
 import org.httpserver.server.HttpMethod;
 
 import java.util.List;
@@ -14,6 +16,29 @@ public class RedirectHandler implements Handler {
 
     @Override
     public Response handleResponse(Request request) {
-        return null;
+        String CRLF = "\r\n";
+
+        String responseStatusLine = handleStatusLine(request) + CRLF;
+        String responseHeaders = handleHeaders(request) + CRLF + CRLF;
+        String responseBody = handleBody();
+
+        ResponseBuilder responseBuilder = new ResponseBuilder();
+        return responseBuilder.buildResponse(responseStatusLine, responseHeaders, responseBody);
+    }
+
+    private String handleStatusLine(Request request) {
+        String SP = " ";
+        String statusCode = StatusCode.MOVED_PERMANENTLY.getStatusCode();
+        String statusText = String.valueOf(StatusCode.MOVED_PERMANENTLY).replace("_"," ");
+
+        return request.getHttpVersion() + SP + statusCode + SP + statusText;
+    }
+
+    private String handleHeaders(Request request) {
+        return "Location: http://127.0.0.1:5000/simple_get";
+    }
+
+    private String handleBody() {
+        return "";
     }
 }
--- src/test/java/org/httpserver/handler/RedirectHandlerTest.java
+++ src/test/java/org/httpserver/handler/RedirectHandlerTest.java
@@ -1,6 +1,10 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 class RedirectHandlerTest {
@@ -11,5 +15,24 @@ class RedirectHandlerTest {
 
         assertTrue(redirectHandler.allowedHttpMethods().contains("GET"));
         assertEquals(1, redirectHandler.allowedHttpMethods().size());
+    }
+
+    @Test
+    void returnsResponseWithStatusLineAndBodyOnly() {
+        LinkedHashMap<String, String> requestLineStub = new LinkedHashMap<String, String>() {{
+            put("httpVersion", "HTTP/1.1");
+            put("httpMethod", "GET");
+            put("requestTarget", "/redirect");
+        }};
+        LinkedHashMap<String, String> requestHeadersStub = new LinkedHashMap<>();
+        String requestBodyStub = "";
+        Request requestMock = new Request(requestLineStub, requestHeadersStub, requestBodyStub);
+        RedirectHandler redirectHandler = new RedirectHandler();
+
+        Response actualResponse = redirectHandler.handleResponse(requestMock);
+
+        assertEquals("HTTP/1.1 301 MOVED PERMANENTLY\r\n", actualResponse.getResponseStatusLine());
+        assertEquals("Location: http://127.0.0.1:5000/simple_get\r\n\r\n", actualResponse.getResponseHeaders());
+        assertEquals(requestBodyStub, actualResponse.getResponseBody());
     }
 }
