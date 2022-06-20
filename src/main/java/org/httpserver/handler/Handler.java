8ae4c053ac55fe0fce65bc7273e125822e8925b1
fi-ya <safiaali@hotmail.com>
2022-06-20 17:21:37 +0100

    refactor: remove unused code in Handler

--- src/main/java/org/httpserver/handler/Handler.java
+++ src/main/java/org/httpserver/handler/Handler.java
@@ -11,24 +11,4 @@ public interface Handler {
     List<String> allowedHttpMethods();
 
     Response handleResponse(Request request);
-
-    default String handleStatusLine(Request request, StatusCode statusText) {
-        String httpVersion = request.getHttpVersion();
-        String statusCode = statusText.getStatusCode();
-        String statusTextString = String.valueOf(statusText).replace("_", " ");
-
-        return String.format("%s %s %s", httpVersion, statusCode, statusTextString);
-    }
-
-    default String handleHeaders(String headerName, String headerValue) {
-        return isNoHeader(headerName, headerValue) ? "" : headerName + ": " + headerValue;
-    }
-
-    private boolean isNoHeader(String headerName, String headerValue) {
-        return Objects.equals(headerName, "") && Objects.equals(headerValue, "");
-    }
-
-    default String handleBody(String responseBody) {
-        return Objects.equals(responseBody, "") ? "" : responseBody;
-    }
 }
