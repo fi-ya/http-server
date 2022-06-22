e7894186cb24264e766d06b0f5f6a410bd4225fe
fi-ya <safiaali@hotmail.com>
2022-06-22 10:59:57 +0100

    test: html_response returnsResponse_withStatusLineAndBody_withText

--- src/main/java/org/httpserver/handler/HtmlHandler.java
+++ src/main/java/org/httpserver/handler/HtmlHandler.java
@@ -2,6 +2,8 @@ package org.httpserver.handler;
 
 import org.httpserver.request.Request;
 import org.httpserver.response.Response;
+import org.httpserver.response.ResponseBuilder;
+import org.httpserver.response.StatusCode;
 import org.httpserver.server.HttpMethod;
 
 import java.util.List;
@@ -14,6 +16,11 @@ public class HtmlHandler implements Handler{
 
     @Override
     public Response handleResponse(Request request) {
-        return null;
+        return new ResponseBuilder()
+                .withStatusCode(StatusCode.OK)
+                .withHeaderName("Content-Type")
+                .withHeaderValue("text/html;charset=utf-8")
+                .withBody("<html><body><p>HTML Response</p></body></html>")
+                .build();
     }
 }
--- src/test/java/org/httpserver/handler/HtmlHandlerTest.java
+++ src/test/java/org/httpserver/handler/HtmlHandlerTest.java
@@ -1,6 +1,10 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +17,24 @@ class HtmlHandlerTest {
         assertTrue(htmlHandler.allowedHttpMethods().contains("GET"));
         assertEquals(1, htmlHandler.allowedHttpMethods().size());
     }
+
+    @Test
+    void returnsResponse_withStatusLineAndBody_withText() {
+        LinkedHashMap<String, String> requestLineStub = new LinkedHashMap<>() {{
+            put("httpVersion", "HTTP/1.1");
+            put("httpMethod", "GET");
+            put("requestTarget", "/html_response");
+        }};
+       HtmlHandler htmlHandler = new HtmlHandler();
+
+        Response actualResponse = htmlHandler.handleResponse(new Request(requestLineStub, new LinkedHashMap<>(), ""));
+
+        assertEquals("HTTP/1.1 200 OK\r\n", actualResponse.getResponseStatusLine());
+        assertTrue(actualResponse.getResponseHeaders().contains("Content-Type"));
+        assertTrue(actualResponse.getResponseHeaders().contains("text/html;charset=utf-8"));
+        assertEquals("<html><body><p>HTML Response</p></body></html>", actualResponse.getResponseBody());
+    }
+
+
 
 }
