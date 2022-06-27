d6b66adaa5a37a38dfa5b17e82426e44fb239a22
fi-ya <safiaali@hotmail.com>
2022-06-27 13:13:06 +0100

    test: ResponseHeaderMaker returns allowHeader for OptionsHandler, OptionsHandlerTwo & MethodNotAllowedHandler

--- src/main/java/org/httpserver/response/ResponseHeaderMaker.java
+++ src/main/java/org/httpserver/response/ResponseHeaderMaker.java
@@ -1,6 +1,9 @@
 package org.httpserver.response;
 
+import org.httpserver.server.HttpMethod;
+
 import java.sql.Array;
+import java.util.StringJoiner;
 
 public class ResponseHeaderMaker {
 
@@ -9,6 +12,17 @@ public class ResponseHeaderMaker {
     }
     public static String[] contentLengthHeader(String body) {
         return new String[]{ResponseHeaderName.CONTENT_LENGTH, String.valueOf(body.length())};
+    }
+
+    public static String[] allowHeader(HttpMethod ... args){
+        StringBuilder allowedHttpMethods = new StringBuilder();
+
+        for (HttpMethod arg : args){
+            allowedHttpMethods.append(arg).append(", ");
+        }
+       String finalAllowedHeaders = allowedHttpMethods.substring(0, allowedHttpMethods.length()-2);
+
+        return new String[] {ResponseHeaderName.ALLOW, String.valueOf(finalAllowedHeaders)};
     }
 
 }
--- src/test/java/org/httpserver/response/ResponseHeaderMakerTest.java
+++ src/test/java/org/httpserver/response/ResponseHeaderMakerTest.java
@@ -1,7 +1,9 @@
 package org.httpserver.response;
 
+import org.httpserver.server.HttpMethod;
 import org.junit.jupiter.api.Test;
 
+import static org.httpserver.server.HttpMethod.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseHeaderMakerTest {
@@ -23,6 +25,28 @@ class ResponseHeaderMakerTest {
         String[] expectedHeader = new String[] {"Content-Length", "11"};
 
         String [] actualHeader = ResponseHeaderMaker.contentLengthHeader("Hello world");
+
+        assertEquals(expectedHeader[0], actualHeader[0]);
+        assertEquals(expectedHeader[1], actualHeader[1]);
+    }
+
+    @Test
+    void returnsAllowHeaderStringArray_withGetHeadOptionsOnly(){
+
+        String[] expectedHeader = new String[] {"Allow", "GET, HEAD, OPTIONS"};
+
+        String [] actualHeader = ResponseHeaderMaker.allowHeader(GET, HEAD, OPTIONS);
+
+        assertEquals(expectedHeader[0], actualHeader[0]);
+        assertEquals(expectedHeader[1], actualHeader[1]);
+    }
+
+    @Test
+    void returnsAllowHeaderStringArray_withGetHeadOptionsPostPut(){
+
+        String[] expectedHeader = new String[] {"Allow", "GET, HEAD, OPTIONS, POST, PUT"};
+
+        String [] actualHeader = ResponseHeaderMaker.allowHeader(GET, HEAD, OPTIONS, POST, PUT);
 
         assertEquals(expectedHeader[0], actualHeader[0]);
         assertEquals(expectedHeader[1], actualHeader[1]);
