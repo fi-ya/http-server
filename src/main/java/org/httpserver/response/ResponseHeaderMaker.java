37ec3a08dd4e1ea59c325bcfe1b8834bd38fdf56
fi-ya <safiaali@hotmail.com>
2022-06-27 13:25:20 +0100

    test: ResponseHeaderMaker returnsHtmlTextHeaderStringArray

--- src/main/java/org/httpserver/response/ResponseHeaderMaker.java
+++ src/main/java/org/httpserver/response/ResponseHeaderMaker.java
@@ -2,30 +2,32 @@ package org.httpserver.response;
 
 import org.httpserver.server.HttpMethod;
 
-import java.sql.Array;
-import java.util.StringJoiner;
-
 public class ResponseHeaderMaker {
 
-    public static String[] plainTextHeader(){
+    public static String[] plainTextHeader() {
         return new String[]{ResponseHeaderName.CONTENT_TYPE, ContentType.TEXT.getValue()};
     }
+
     public static String[] contentLengthHeader(String body) {
         return new String[]{ResponseHeaderName.CONTENT_LENGTH, String.valueOf(body.length())};
     }
 
-    public static String[] allowHeader(HttpMethod ... args){
+    public static String[] allowHeader(HttpMethod... args) {
         StringBuilder allowedHttpMethods = new StringBuilder();
 
-        for (HttpMethod arg : args){
+        for (HttpMethod arg : args) {
             allowedHttpMethods.append(arg).append(", ");
         }
-       String finalAllowedHeaders = allowedHttpMethods.substring(0, allowedHttpMethods.length()-2);
+        String finalAllowedHeaders = allowedHttpMethods.substring(0, allowedHttpMethods.length() - 2);
 
-        return new String[] {ResponseHeaderName.ALLOW, String.valueOf(finalAllowedHeaders)};
+        return new String[]{ResponseHeaderName.ALLOW, String.valueOf(finalAllowedHeaders)};
     }
 
     public static String[] locationHeader(String newLocation) {
-        return new String[] {ResponseHeaderName.LOCATION, newLocation};
+        return new String[]{ResponseHeaderName.LOCATION, newLocation};
+    }
+
+    public static String[] htmlTextHeader() {
+        return new String[]{ResponseHeaderName.CONTENT_TYPE, ContentType.HTML.getValue()};
     }
 }
