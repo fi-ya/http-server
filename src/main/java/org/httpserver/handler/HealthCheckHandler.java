d4cc56cbb4485bbfc3c3829b57582a279e1f6212
fi-ya <safiaali@hotmail.com>
2022-06-29 12:43:35 +0100

    add: health-check.html in webapp dir

--- src/main/java/org/httpserver/handler/HealthCheckHandler.java
+++ src/main/java/org/httpserver/handler/HealthCheckHandler.java
@@ -2,10 +2,15 @@ package org.httpserver.handler;
 
 import org.httpserver.request.Request;
 import org.httpserver.response.Response;
+import org.httpserver.response.ResponseBuilder;
+import org.httpserver.response.ResponseHeaderMaker;
 import org.httpserver.server.HttpMethod;
 
 import java.util.List;
 
+import static org.httpserver.response.ResponseHeaderMaker.*;
+import static org.httpserver.response.StatusCode.OK;
+import static org.httpserver.response.TextConstants.textResponse;
 import static org.httpserver.server.HttpMethod.GET;
 
 public class HealthCheckHandler implements Handler{
@@ -16,6 +21,15 @@ public class HealthCheckHandler implements Handler{
 
     @Override
     public Response handleResponse(Request request) {
-        return null;
+        // get .html file = read public dir
+        //String get html body = read html file
+        // pass html body into ResponseBuilder
+
+        return new ResponseBuilder()
+                .withStatusCode(OK)
+                .withHeader(htmlTextHeader())
+//                .withHeader(contentLengthHeader(htmlResponse))
+//                .withBody(htmlResponse)
+                .build();
     }
 }
