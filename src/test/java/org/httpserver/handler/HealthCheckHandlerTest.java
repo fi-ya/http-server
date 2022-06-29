18251f860e206b8b7989749e7bdd830362a53c7e
fi-ya <safiaali@hotmail.com>
2022-06-29 12:43:35 +0100

    add: health-check.html in webapp dir

--- src/test/java/org/httpserver/handler/HealthCheckHandlerTest.java
+++ src/test/java/org/httpserver/handler/HealthCheckHandlerTest.java
@@ -1,6 +1,11 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.request.RequestLine;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
 
 import static org.httpserver.server.HttpMethod.GET;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,4 +20,16 @@ class HealthCheckHandlerTest {
         assertEquals(1, healthCheckHandler.allowedHttpMethods().size());
     }
 
+    @Test
+    void returnsResponse_withStatusLineAndBody_containingHtmlFile() {
+        RequestLine mockRequestLine = new RequestLine(GET, "/health-check.html", "HTTP/1.1");
+        HealthCheckHandler healthCheckHandler = new HealthCheckHandler();
+
+        Response actualResponse = healthCheckHandler.handleResponse(new Request(mockRequestLine, new LinkedHashMap<>(), ""));
+
+        assertEquals("HTTP/1.1 200 OK\r\n", actualResponse.getResponseStatusLine());
+        assertTrue(actualResponse.getResponseHeaders().contains("Content-Type"));
+        assertTrue(actualResponse.getResponseHeaders().contains("text/html;charset=utf-8"));
+        assertEquals("<strong>Status:</strong> pass", actualResponse.getResponseBody());
+    }
 }
