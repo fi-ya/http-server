292121fba5e460d1f7879c17b78751f7c6632a98
fi-ya <safiaali@hotmail.com>
2022-06-29 20:36:54 +0100

    test: ImagesHandler returns correct response headers depending on request target

--- src/main/java/org/httpserver/response/ContentType.java
+++ src/main/java/org/httpserver/response/ContentType.java
@@ -3,7 +3,7 @@ package org.httpserver.response;
 public enum ContentType {
     TEXT("text/plain;charset=utf-8"),
     HTML("text/html;charset=utf-8"),
-    JPG("image/jpg"),
+    JPEG("image/jpeg"),
     PNG("image/png"),
     GIF("image/gif");
 
