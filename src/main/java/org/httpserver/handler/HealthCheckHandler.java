710fa6ee247277f6a3dac442092b2f78cffc521f
fi-ya <safiaali@hotmail.com>
2022-06-30 15:46:29 +0100

    test: HealthCheckHandler returnsResponse_withStatusLineAndBody_containingHtmlFile()

--- src/main/java/org/httpserver/handler/HealthCheckHandler.java
+++ src/main/java/org/httpserver/handler/HealthCheckHandler.java
@@ -6,6 +6,10 @@ import org.httpserver.response.ResponseBuilder;
 import org.httpserver.response.ResponseHeaderMaker;
 import org.httpserver.server.HttpMethod;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import static org.httpserver.response.ResponseHeaderMaker.*;
@@ -20,16 +24,33 @@ public class HealthCheckHandler implements Handler{
     }
 
     @Override
-    public Response handleResponse(Request request) {
-        // get .html file = read public dir
-        //String get html body = read html file
-        // pass html body into ResponseBuilder
+    public Response handleResponse(Request request) throws IOException {
+        String htmlResource = request.getRequestTarget();
+        String body = getResponseBody(htmlResource);
 
         return new ResponseBuilder()
                 .withStatusCode(OK)
                 .withHeader(htmlTextHeader())
-//                .withHeader(contentLengthHeader(htmlResponse))
-//                .withBody(htmlResponse)
+                .withHeader(contentLengthHeader(body))
+                .withBody(body)
                 .build();
     }
+
+    private String getResponseBody(String htmlResource) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(htmlResource);
+        return convertHtmlFileToString(inputStream);
+    }
+
+    private String convertHtmlFileToString(InputStream inputStream) throws IOException {
+        StringBuilder htmlStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                htmlStringBuilder.append(line).append("\n");
+            }
+        }
+        return htmlStringBuilder.toString().trim();
+    }
+
+
 }
