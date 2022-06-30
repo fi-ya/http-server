e71cca9a9fc12a20a0fff0eb3ef3d1525ff6ff3d
fi-ya <safiaali@hotmail.com>
2022-06-30 15:26:58 +0100

    refactor: remove unused code

--- src/main/java/org/httpserver/response/ResponseBuilder.java
+++ src/main/java/org/httpserver/response/ResponseBuilder.java
@@ -1,7 +1,5 @@
 package org.httpserver.response;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -55,31 +53,4 @@ public class ResponseBuilder {
     private String buildBody() {
         return Objects.equals(body, "") ? "" : body;
     }
-
-//    public byte[] responseStringToBytes() throws IOException {
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        outputStream.write(getStatusLineBytes());
-//        outputStream.write(getHeaderBytes());
-//        outputStream.write(getBodyBytes());
-//
-//        return outputStream.toByteArray();
-//    }
-//
-//    private byte[] getStatusLineBytes() {
-//
-//        return buildStatusLine().getBytes();
-//    }
-//
-//    private byte[] getHeaderBytes() {
-//        return buildHeaders().getBytes();
-//    }
-//
-//    private byte[] getBodyBytes() {
-//        byte[] bodyBytes = new byte[0];
-//        if (buildBody() != null) {
-//            bodyBytes = buildBody().getBytes();
-//        }
-//        return bodyBytes;
-//    }
-
 }
