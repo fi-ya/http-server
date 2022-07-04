04e75597d2f5d219f5c54fd7c7f83d04f0f8797c
fi-ya <safiaali@hotmail.com>
2022-07-04 13:28:12 +0100

    test: XmlHandler returnsResponse_withStatusLineAndBody_withXml hardcoded

--- src/main/java/org/httpserver/handler/XmlHandler.java
+++ src/main/java/org/httpserver/handler/XmlHandler.java
@@ -2,10 +2,14 @@ package org.httpserver.handler;
 
 import org.httpserver.request.Request;
 import org.httpserver.response.Response;
+import org.httpserver.response.ResponseBuilder;
 import org.httpserver.server.HttpMethod;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.httpserver.response.ResponseHeaderMaker.*;
+import static org.httpserver.response.StatusCode.OK;
 import static org.httpserver.server.HttpMethod.GET;
 
 public class XmlHandler implements Handler{
@@ -16,6 +20,14 @@ public class XmlHandler implements Handler{
 
     @Override
     public Response handleResponse(Request request) {
-        return null;
+        String body = "<note><body>XML Response</body></note>";
+        byte[] bodyByte = body.getBytes();
+
+        return new ResponseBuilder()
+                .withStatusCode(OK)
+                .withHeader(xmlHeader())
+                .withHeader(contentLengthHeader(body))
+                .withBodyByte(bodyByte)
+                .build();
     }
 }
--- src/test/java/org/httpserver/handler/XmlHandlerTest.java
+++ src/test/java/org/httpserver/handler/XmlHandlerTest.java
@@ -1,6 +1,11 @@
 package org.httpserver.handler;
 
+import org.httpserver.request.Request;
+import org.httpserver.request.RequestLine;
+import org.httpserver.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
 
 import static org.httpserver.server.HttpMethod.GET;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +18,19 @@ class XmlHandlerTest {
 
         assertTrue(xmlHandler.allowedHttpMethods().contains(GET));
         assertEquals(1, xmlHandler.allowedHttpMethods().size());
+    }
+
+    @Test
+    void returnsResponse_withStatusLineAndBody_withXml() {
+        RequestLine mockRequestLine = new RequestLine(GET, "/xml_response", "HTTP/1.1");
+        XmlHandler xmlHandler = new XmlHandler();
+
+        Response actualResponse = xmlHandler.handleResponse(new Request(mockRequestLine, new LinkedHashMap<>(), ""));
+
+        assertEquals("HTTP/1.1 200 OK\r\n", actualResponse.getResponseStatusLine());
+        assertTrue(actualResponse.getResponseHeaders().contains("Content-Type"));
+        assertTrue(actualResponse.getResponseHeaders().contains("application/xml;charset=utf-8"));
+        assertEquals("<note><body>XML Response</body></note>", new String(actualResponse.getBodyBytes()));
     }
 
 }
