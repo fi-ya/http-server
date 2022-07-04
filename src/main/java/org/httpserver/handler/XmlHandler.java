7a572fa91e707855226949483942147c2d81f5bd
fi-ya <safiaali@hotmail.com>
2022-07-04 14:52:58 +0100

    refactor: remove unused imports

--- src/main/java/org/httpserver/handler/XmlHandler.java
+++ src/main/java/org/httpserver/handler/XmlHandler.java
@@ -5,14 +5,14 @@ import org.httpserver.response.Response;
 import org.httpserver.response.ResponseBuilder;
 import org.httpserver.server.HttpMethod;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.httpserver.response.ResponseHeaderMaker.*;
+import static org.httpserver.response.ResponseHeaderMaker.contentLengthHeader;
+import static org.httpserver.response.ResponseHeaderMaker.xmlHeader;
 import static org.httpserver.response.StatusCode.OK;
 import static org.httpserver.server.HttpMethod.GET;
 
-public class XmlHandler implements Handler{
+public class XmlHandler implements Handler {
     @Override
     public List<HttpMethod> allowedHttpMethods() {
         return List.of(GET);
