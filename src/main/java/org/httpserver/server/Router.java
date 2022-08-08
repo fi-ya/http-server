f7184e4917bb077acc9f0dc41d0890a326b9ef84
Safia <69358550+fi-ya@users.noreply.github.com>
2022-08-08 15:47:41 +0100

    Add types to createResourceAndHandlerMap hashmap

--- src/main/java/org/httpserver/server/Router.java
+++ src/main/java/org/httpserver/server/Router.java
@@ -15,7 +15,7 @@ public class Router {
     }
 
     public Map<String, Handler> createResourceAndHandlerMap() {
-        return new HashMap<>() {{
+        return new HashMap<String, Handler>() {{
             put("/simple_get", new SimpleGetHandler());
             put("/simple_get_with_body", new SimpleGetWithBodyHandler());
             put("/head_request", new HeadRequestHandler());
