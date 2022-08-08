682c1e345f98510467fa91ab501b2d8ec23a123a
fi-ya <safiaali@hotmail.com>
2022-08-08 20:12:42 +0100

    Remove logs in server wrapper

--- src/main/java/org/httpserver/server/ServerWrapper.java
+++ src/main/java/org/httpserver/server/ServerWrapper.java
@@ -1,23 +1,23 @@
 package org.httpserver.server;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerWrapper {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ServerSocket createServerSocket(int portNumber) throws IOException {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(portNumber);
-            logger.info("Server socket connection created. Listening on port " + portNumber);
+//            logger.info("Server socket connection created. Listening on port " + portNumber);
         } catch (IOException ioException) {
             ioException.printStackTrace();
-            logger.error("Server connection failed, cannot listen for client request on port " + portNumber);
+//            logger.error("Server connection failed, cannot listen for client request on port " + portNumber);
             System.exit(1);
         }
         return serverSocket;
@@ -27,10 +27,10 @@ public class ServerWrapper {
         Socket clientSocket = null;
         try {
             clientSocket = serverSocket.accept();
-            logger.info("Client connection successful: new client socket port number is " + clientSocket.getPort());
+//            logger.info("Client connection successful: new client socket port number is " + clientSocket.getPort());
         } catch (IOException ioException) {
             ioException.printStackTrace();
-            logger.error("Connection unsuccessful: Server failed to accept client connection");
+//            logger.error("Connection unsuccessful: Server failed to accept client connection");
         }
         return clientSocket;
     }
