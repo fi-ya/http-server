5edd355d90bc3144ac6545654e7e4595d0fd21be
fi-ya <safiaali@hotmail.com>
2022-08-08 20:09:27 +0100

    Remove logs

--- src/main/java/org/httpserver/server/Server.java
+++ src/main/java/org/httpserver/server/Server.java
@@ -5,8 +5,8 @@ import org.httpserver.handler.Handler;
 import org.httpserver.request.Request;
 import org.httpserver.request.RequestParser;
 import org.httpserver.response.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,9 +16,9 @@ import java.net.Socket;
 
 public class Server {
     public static int portNumber = 5000;
-    private final Logger logger = LoggerFactory.getLogger("org.httpserver.server.Server");
-
-    private final Logger loggerReqRes = LoggerFactory.getLogger("loggerReqRes");
+//    private final Logger logger = LoggerFactory.getLogger("org.httpserver.server.Server");
+//
+//    private final Logger loggerReqRes = LoggerFactory.getLogger("loggerReqRes");
 
     public Server(int portNumber) {
         this.portNumber = portNumber;
@@ -39,15 +39,15 @@ public class Server {
             RequestParser requestParser = new RequestParser();
 
             Request request = requestParser.parseRequest(clientRequestInputStream);
-            loggerReqRes.info("Request: " + request.stringFormatRequest());
+//            loggerReqRes.info("Request: " + request.stringFormatRequest());
 
             Router router = new Router();
             Handler handler = router.getHandler(request);
 
-            logger.info(handler.getClass().getSimpleName() + ": building a response");
+//            logger.info(handler.getClass().getSimpleName() + ": building a response");
 
             Response response = handler.handleResponse(request);
-            loggerReqRes.info("Response: " + response.stringFormatResponse());
+//            loggerReqRes.info("Response: " + response.stringFormatResponse());
 
             clientHandler.processSendResponse(response);
 
